abad4cc1fb6467db1142eca36a4464dea9143b23
James Batsell <jamesbatsell05@gmail.com>
2019-10-04 08:07:53 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -1,8 +1,8 @@
 class EmailParser
   attr_accessor :email
-  def initialize(email)
-    @email = email
-    email
+  
+  def initialize(emails)
+    @emails = emails
   end
   def email
     email = email.split.strip(" ", ",")
